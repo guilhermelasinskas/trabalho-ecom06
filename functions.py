3a4e0adf6cc4f806b466124d287983433e2d8057
Guilherme Lasinskas <guilherme.lasinskas@gmail.com>
2021-12-11 19:14:54 -0300

    Validators completed

--- functions.py
+++ functions.py
@@ -21,13 +21,16 @@ def lexical_analyzer(string_main, stopc):
 def entity_lexical_analyser(command, token1, token2):
 
     if(middle_layer.validate_command(command) == 0):
-        print("CRIAR")
-        assert (middle_layer.validate_folder(token1) or middle_layer.validade_path(token1))
-        middle_layer.create(token1, token2)
+        assert (middle_layer.validate_folder(token1) or middle_layer.validate_path(token1))
+
+        if middle_layer.create(token1, token2):
+            print("O arquivo foi criado")
+        else:
+            print("O arquivo não foi criado")
 
     elif(middle_layer.validate_command(command) == 1):
         print("GUARDAR")
-        assert (middle_layer.validade_file(token1) and middle_layer.validate_folder(token2))
+        assert (middle_layer.validate_file(token1) and middle_layer.validate_folder(token2))
         middle_layer.store(token1, token2)
 
     else:
